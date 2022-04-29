f49c08512f9e040bfcf321db0be6cc0d33b20d30
RubenBarrS <82640613+RubenBarrS@users.noreply.github.com>
2022-04-29 18:04:28 +0200

    Update ajax.js

--- Ruben/Ej4/js/ajax.js
+++ Ruben/Ej4/js/ajax.js
@@ -13,23 +13,49 @@ function cargarProfe(){
 }
 
 function Myprofes(xml)
-{
+{	
 	var obj_div=document.getElementsByTagName('div')[0];
-	var xhr = xml.responseXML;
-	var obj_profesores= xhr.getElementsByTagName("profesor");
-
+	const arr = [];
+	var xhr=xml.responseXML;
+	var obj_profesores = xhr.getElementsByTagName("profesor");
 	for(i=0;i<obj_profesores.length;i++)
 	{
 		var obj_p=document.createElement("p");
-		var nombre= obj_profesores[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue;
-		var lista_asi = obj_profesores[i].getElementsByTagName('asignatura');
-		var texto="";
-			for(j=0;j<lista_asi.length; j++)
-			{
-				var asi = lista_asi[j].childNodes[0].nodeValue;
-				texto=asi +" "+ texto;
-			}
-		obj_p.innerHTML="NOMBRE: "+nombre + " Asignaturas: "+texto;
+		var nombre=obj_profesores[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue;
+		arr[i]=new Object();
+		arr[i].nombre=nombre;
+		obj_p.innerHTML=arr[i].nombre;
 		obj_div.appendChild(obj_p);
+
+		var obj_asi=obj_profesores[i].getElementsByTagName('asignatura');
+		var asig_texto="";
+		for(j=0;j<obj_asi.length;j++)
+		{
+			var asi=obj_asi[j].childNodes[0].nodeValue;
+			asig_texto=asig_texto+"\n"+asi;
+		}
+		arr[i].asig=asig_texto;
+
 	}
+	crearLista(arr);
+}
+
+
+function crearLista(lista_asignatura)
+{
+
+	var lista=document.createElement("ul");
+	for(i=0;lista_asignatura.length;i++)
+	{
+		var asg=lista_asignatura[i].asig;
+		var asig=asg.split("\n");
+		for(j=0;j<asig.length;j++){
+			var obj_li=document.createElement("li");
+			obj_li.innerHTML=asig[j];
+			lista.appendChild(obj_li);
+		}
+		document.getElementsByTagName("p")[0].appendChild(lista);
+	}
+
+
 }
