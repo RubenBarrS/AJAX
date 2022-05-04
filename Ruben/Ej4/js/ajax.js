6eb1be2429a55329368a26563e6eaf71fadcd97f
RubenBarrS <82640613+RubenBarrS@users.noreply.github.com>
2022-05-04 13:43:10 +0200

    Update ajax.js

--- Ruben/Ej4/js/ajax.js
+++ Ruben/Ej4/js/ajax.js
@@ -5,7 +5,7 @@ function cargarProfe(){
 	{
 		if(ajax.readyState==4 && ajax.status==200)
 		{
-			Myprofes(ajax);
+			Myprofes(ajax.responseXML);
 		}
 	}
 	ajax.open("Get","profesor.xml");
@@ -14,48 +14,48 @@ function cargarProfe(){
 
 function Myprofes(xml)
 {	
-	var obj_div=document.getElementsByTagName('div')[0];
+	
 	const arr = [];
-	var xhr=xml.responseXML;
-	var obj_profesores = xhr.getElementsByTagName("profesor");
+	var obj_profesores = xml.getElementsByTagName("profesor");
 	for(i=0;i<obj_profesores.length;i++)
 	{
-		var obj_p=document.createElement("p");
 		var nombre=obj_profesores[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue;
 		arr[i]=new Object();
 		arr[i].nombre=nombre;
-		obj_p.innerHTML=arr[i].nombre;
-		obj_div.appendChild(obj_p);
 
 		var obj_asi=obj_profesores[i].getElementsByTagName('asignatura');
-		var asig_texto="";
+		arr[i].asignaturas=[];
 		for(j=0;j<obj_asi.length;j++)
 		{
+			
 			var asi=obj_asi[j].childNodes[0].nodeValue;
-			asig_texto=asig_texto+"\n"+asi;
+			arr[i].asignaturas[j]=asi;
 		}
-		arr[i].asig=asig_texto;
+	
 
 	}
 	crearLista(arr);
 }
 
 
-function crearLista(lista_asignatura)
+function crearLista(profesores)
 {
-
-	var lista=document.createElement("ul");
-	for(i=0;lista_asignatura.length;i++)
+	var obj_div=document.getElementById('contenedor-profes');
+	for(i=0;i<profesores.length;i++)
 	{
-		var asg=lista_asignatura[i].asig;
-		var asig=asg.split("\n");
-		for(j=0;j<asig.length;j++){
+		var obj_p=document.createElement("p");
+		var lista=document.createElement("ul");
+		var nombre=profesores[i].nombre;
+		obj_p.innerHTML=nombre;
+		var lista_asig = profesores[i].asignaturas;
+		for(j=0;j<lista_asig.length;j++)
+		{
 			var obj_li=document.createElement("li");
-			obj_li.innerHTML=asig[j];
+			obj_li.innerHTML=lista_asig[j];
 			lista.appendChild(obj_li);
+			
 		}
-		document.getElementsByTagName("p")[0].appendChild(lista);
+		obj_div.appendChild(obj_p);
+		obj_div.appendChild(lista);
 	}
-
-
 }
